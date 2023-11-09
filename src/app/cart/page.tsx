a5825820f5effb49c64c1ec4d5e173d03a3c092e
Rubayet-billah <rubayetbillah909@gmail.com>
2023-11-10 03:36:27 +0600

    dashboard layout folder created

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,7 +1,15 @@
+"use client";
 import CartPage from "@/components/cart/CartPage";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 const Cart = () => {
+  const router = useRouter();
+  const user = "Jhon doe";
+  if (!user) {
+    router.push("/auth/login");
+    return;
+  }
   return (
     <div>
       <CartPage />

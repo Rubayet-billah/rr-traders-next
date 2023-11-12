86ac9df24cd955f6c6f0b92d07de7d708c21dadb
Rubayet-billah <rubayetbillah909@gmail.com>
2023-11-13 01:04:03 +0600

    login page refactored

--- src/components/authentication/LoginPage.tsx
+++ src/components/authentication/LoginPage.tsx
@@ -1,42 +1,26 @@
+import React from "react";
 import SectionHeading from "../utils/SectionHeading";
+import FormInput from "../utils/forms/FormInput";
 
 const LoginPage = () => {
   return (
     <div className="container mx-auto my-12">
-      <SectionHeading title="Login" subTitle="Login with your credintials" />
+      <SectionHeading title="Login" subTitle="Login with your credentials" />
       <form className="max-w-md mx-auto mt-5 lg:mt-12">
-        <div className="relative z-0 w-full mb-6 group">
-          <input
-            type="email"
-            name="floating_email"
-            id="floating_email"
-            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-            placeholder=" "
-            required
-          />
-          <label
-            htmlFor="floating_email"
-            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-          >
-            Email address <span className="text-red-600">*</span>
-          </label>
-        </div>
-        <div className="relative z-0 w-full mb-6 group">
-          <input
-            type="password"
-            name="floating_password"
-            id="floating_password"
-            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-            placeholder=" "
-            required
-          />
-          <label
-            htmlFor="floating_password"
-            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-          >
-            Password <span className="text-red-600">*</span>
-          </label>
-        </div>
+        <FormInput
+          type="email"
+          name="floating_email"
+          id="floating_email"
+          label="Email address"
+          required
+        />
+        <FormInput
+          type="password"
+          name="floating_password"
+          id="floating_password"
+          label="Password"
+          required
+        />
         <button
           type="submit"
           className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded text-sm w-full px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800 uppercase"
---
+++ src/components/utils/button/ReuseableButton.tsx
@@ -0,0 +1,14 @@
+// import React from "react";
+
+// const ReuseableButton = ({ type, children }) => {
+//   return (
+//     <button
+//       type="submit"
+//       className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded text-sm w-full px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800 uppercase"
+//     >
+//       Login
+//     </button>
+//   );
+// };
+
+// export default ReuseableButton;

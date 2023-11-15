7fdfccdcfb54137110869791680935444507632d
Rubayet-billah <rubayetbillah909@gmail.com>
2023-11-15 23:22:29 +0600

    form value collected

--- src/components/authentication/LoginPage.tsx
+++ src/components/authentication/LoginPage.tsx
@@ -1,25 +1,43 @@
-import React from "react";
+"use client";
+import { useForm, SubmitHandler } from "react-hook-form";
 import SectionHeading from "../utils/SectionHeading";
 import FormInput from "../utils/forms/FormInput";
 
+type Inputs = {
+  floating_email: string;
+  floating_password: string;
+};
+
 const LoginPage = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+
   return (
     <div className="container mx-auto my-12">
       <SectionHeading title="Login" subTitle="Login with your credentials" />
-      <form className="max-w-md mx-auto mt-5 lg:mt-12">
+      <form
+        className="max-w-md mx-auto mt-5 lg:mt-12"
+        onSubmit={handleSubmit(onSubmit)}
+      >
         <FormInput
           type="email"
-          name="floating_email"
+          name="email"
           id="floating_email"
           label="Email address"
           required
+          register={register}
         />
         <FormInput
           type="password"
-          name="floating_password"
+          name="password"
           id="floating_password"
           label="Password"
           required
+          register={register}
         />
         <button
           type="submit"
--- src/components/utils/forms/FormInput.tsx
+++ src/components/utils/forms/FormInput.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { ChangeEvent } from "react";
 
 type FormInputProps = {
   type: string;
@@ -8,6 +8,8 @@ type FormInputProps = {
   label: string;
   required?: boolean;
   pattern?: string;
+  onChange?: (value: string) => void; // Add onChange prop
+  register?: any;
 };
 
 const FormInput: React.FC<FormInputProps> = ({
@@ -18,21 +20,23 @@ const FormInput: React.FC<FormInputProps> = ({
   label,
   required,
   pattern,
+  register,
 }) => {
   return (
     <div className="relative z-0 w-full mb-6 group">
       <input
         type={type}
-        name={name}
         id={id}
         className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
         placeholder={placeholder || " "}
-        required={required || false}
         pattern={pattern}
+        {...register(name, { required })}
       />
       <label
         htmlFor={id}
-        className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+        className={`peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] ${
+          required ? "peer-focus:left-0 peer-focus:text-blue-600" : ""
+        } peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6`}
       >
         {label} {required && <span className="text-red-600">*</span>}
       </label>

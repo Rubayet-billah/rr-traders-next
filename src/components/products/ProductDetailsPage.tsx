a7b72b3fb32988f80bda0ceedf0b96edf7496d8d
Rubayet-billah <rubayetbillah909@gmail.com>
2023-11-25 02:11:30 +0600

    reusuable forms and modal implemented

--- src/components/products/ProductDetailsPage.tsx
+++ src/components/products/ProductDetailsPage.tsx
@@ -12,7 +12,7 @@ const ProductDetailsPage = () => {
             <div className="relative overflow-hidden h-96">
               {/* Product images */}
               <img
-                src="product-image-1.jpg"
+                src="https://images.unsplash.com/photo-1658681906631-f0a96e2d199b?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                 alt="Product Image"
                 className="object-cover h-full w-full"
               />

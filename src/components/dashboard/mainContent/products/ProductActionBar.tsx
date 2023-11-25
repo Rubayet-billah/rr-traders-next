8b96167460f9b12979d07da466c93058fd68640b
Rubayet-billah <rubayetbillah909@gmail.com>
2023-11-25 15:01:02 +0600

    modal content changed

--- src/components/dashboard/mainContent/products/ProductActionBar.tsx
+++ src/components/dashboard/mainContent/products/ProductActionBar.tsx
@@ -1,5 +1,5 @@
-import ModalContent from "@/components/utils/modals/ModalContent";
 import React, { Dispatch, SetStateAction } from "react";
+import AddProductModalContent from "./AddProductModalContent";
 
 interface ProductActionBarProps {
   setModalState: Dispatch<SetStateAction<boolean>>;
@@ -52,7 +52,7 @@ const ProductActionBar = ({
             Add Category
           </button>
           <button
-            onClick={() => handleAddProduct(<ModalContent />)}
+            onClick={() => handleAddProduct(<AddProductModalContent />)}
             className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded text-sm px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800 uppercase block"
           >
             Add Product
